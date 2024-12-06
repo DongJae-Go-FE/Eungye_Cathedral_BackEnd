407b6e84d398a99e40b077e7f2a23d38d26dde37
가동재 <masterforce999@naver.com>
2024-12-06 17:10:41 +0900

    first commit

--- src/swagger.document.ts
+++ src/swagger.document.ts
@@ -5,8 +5,8 @@ export class BaseAPIDocument {
 
   public initializeOptions() {
     return this.builder
-      .setTitle('Swagger Example')
-      .setDescription('Swagger study API description')
+      .setTitle('은계성당 API')
+      .setDescription('은계성당 API입니다.')
       .setVersion('1.0.0')
       .addTag('swagger')
       .build();
